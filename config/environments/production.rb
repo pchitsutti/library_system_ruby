09c9e12244ee492b26fb255239037921f51a6e22
pchitsutti <pitakpong@bananacoding.com>
2019-06-28 16:37:35 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
-    s3_permissions: :private,
-    storage: :s3,
+    :s3_permissions: :private,
+    :storage: :s3,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
