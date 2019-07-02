fa041670738ffeae5437ad8c7101eb942d575c31
pchitsutti <pitakpong@bananacoding.com>
2019-07-02 14:00:10 +0700

    Update 20190702032336_add_owner_and_location.rb

--- db/migrate/20190702032336_add_owner_and_location.rb
+++ db/migrate/20190702032336_add_owner_and_location.rb
@@ -1,6 +1,6 @@
 class AddOwnerAndLocation < ActiveRecord::Migration
   def change
-    # add_column :books, :owner, :int
+    add_column :books, :owner, :int
     add_foreign_key :books, :users, column: :owner
     add_column :books, :location, :string
   end
